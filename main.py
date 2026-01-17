c9816e455d14e923ab969441c8763265ac9332e6
EduardoDosSantosFerreira <eduardo.dsf.dev@gmail.com>
2026-01-17 13:12:44 -0300

    update_index_v1

--- main.py
+++ main.py
@@ -13,14 +13,7 @@ def is_admin():
 
 def relaunch_as_admin():
     params = " ".join(sys.argv)
-    ctypes.windll.shell32.ShellExecuteW(
-        None,
-        "runas",
-        sys.executable,
-        params,
-        None,
-        1
-    )
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, params, None, 1)
     sys.exit()
 
 
